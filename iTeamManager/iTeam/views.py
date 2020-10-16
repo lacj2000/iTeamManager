e67b8321554d41f73b797d82a953ed764239d002
luizjuniordeveloper <luizjuniordeveloper@gmail.com>
2020-10-16 03:07:26 -0300

    pagina de perfil/ incompleta

--- iTeamManager/iTeam/views.py
+++ iTeamManager/iTeam/views.py
@@ -8,7 +8,8 @@ def singin(request):
     return render(request, arquivo, contexto)
 
 def profile(request, user_id):
-    contexto = {'user':{'user_id':5,'name':'Elmo','image_profile':'/media/images/12345678.png' }}
+    contexto = {'user':{'user_id':5,'name':'Elmo','image_profile':'/media/images/12345678.png',
+    'email':'teste1234@teste.tst' }}
     arquivo = 'profile/profile.html'
     return render(request, arquivo, contexto)
 
